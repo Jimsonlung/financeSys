5b2e6ffba27c8e5da056da3d8a4474059bb8bf46
JimsonLung <514989514@qq.com>
2018-06-09 23:07:22 +0800

    测试用例

--- src/com/linqiaoli/test/LoginControllerTest.java
+++ src/com/linqiaoli/test/LoginControllerTest.java
@@ -1,4 +1,23 @@
 package com.linqiaoli.test;
 
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+
 public class LoginControllerTest {
+
+    @Test
+    public void test(){
+        int a = 1+3;
+        Assert.assertThat("不正确", a, is(4));
+    }
+
+    @Ignore
+    @Test
+    public void test2(){
+        int a = 1+3;
+        Assert.assertThat("不正确", a, is(3));
+    }
 }
